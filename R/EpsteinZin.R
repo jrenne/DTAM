ad3b98ed1c9793d73dd0468be5a6264e38ec73c2
jrenne <jprenne@gmail.com>
2025-01-22 22:23:27 +0100

    Update EpsteinZin.R

--- R/EpsteinZin.R
+++ R/EpsteinZin.R
@@ -9,7 +9,7 @@ solve_EZ_SDF <- function(model,psi,Ew=NaN,z_bar_ini=5,
   # - the parameterization of consumption growth, i.e., mu_c0 and mu_c1
   # - the dimension of w, parameter: n_w (used to compute Ew numerically)
   # psi is the log Laplace transform of the state vector w_t; it can itself use
-  #     model as an argument.
+  #     model as an argument (i.e., model contains the process parameterization).
   # Ew is the mean of w_t. If NaN, it is computed numerically.
   # ----------------------------------------------------------------------------
 
@@ -69,30 +69,124 @@ solve_EZ_SDF <- function(model,psi,Ew=NaN,z_bar_ini=5,
     (theta-1)*(kappa0 + kappa1*mu_z0 - mu_z0) - psi_w_alpha$b
   eta1 <- (theta-1)*mu_z1 - psi_w_alpha$a
 
-  return(
-    list(Ew = Ew, Phi = Phi, mu = mu,
-         mu_z0 = mu_z0, mu_z1 = mu_z1, z_bar = z_bar,
-         eta0 = eta0, eta1 = eta1,
-         alpha = alpha,
-         kappa0 = kappa0, kappa1 = kappa1))
+  model_solved <- model
+
+  model_solved$Ew     <- Ew
+  model_solved$Phi    <- Phi
+  model_solved$mu     <- mu
+  model_solved$mu_z0  <- mu_z0
+  model_solved$mu_z1  <- mu_z1
+  model_solved$z_bar  <- z_bar
+  model_solved$eta0   <- eta0
+  model_solved$eta1   <- eta1
+  model_solved$alpha  <- alpha
+  model_solved$kappa0 <- kappa0
+  model_solved$kappa1 <- kappa1
+
+  return(model_solved)
 }
+
+# psi.BansalShaliastovich <- function (u,model){
+#   # This function computes the log Laplace transform associated with
+#   # the model by Bansal and Shaliastovich (2012, RFS)
+#   n <- dim(model$Pi)[1]
+#   q <- dim(model$nu)[1]
+#   K <- dim(u)[2]
+#   u.x <- matrix(u[1:n, ], nrow = n)
+#   u.z <- matrix(u[(n + 1):(n + q), ], nrow = q)
+#   u.epsilon <- matrix(u[(n + q + 1):(n + q + 3), ], nrow = 3)
+#   b <- 0.5 * t(u.x * u.x) %*% matrix(model$chi.0, ncol = 1) +
+#     0.5 * t(u.z * u.z) %*% matrix(model$sigma.w^2, ncol = 1) +
+#     0.5 * ((t(u.epsilon) %*% model$Omega) * t(u.epsilon)) %*%
+#     matrix(1, 3, 1)
+#   a <- rbind(t(model$Pi) %*% u.x, 0.5 * t(model$nu) %*%
+#                model$chi.1 %*% (u.x * u.x) + t(model$nu) %*% u.z, matrix(0,
+#                                                                          3, K))
+#   return(list(a = a,b = b))
+# }
+
+
+
+solve_EZ_stock_return <- function(model,psi,Ew=NaN,z_s_bar_ini=5,
+                                  nb_loop_z_bar=20,nb_loop_mu_z1=100){
+  # This procedure computes an affine approximation to stock return in
+  # the context of Epstein-Zin preferences.
+  # The approach is that of Bansal and Yaron (2004), based on the linearization
+  # of returns à la Campbell and Shiller (1988).
+  # -- Inputs ------------------------------------------------------------------
+  # model_solved is a list containing the model parameters, including:
+  # - the parameterization of preferences, i.e., rho, gamma, delta
+  # - the parameterization of consumption growth, i.e., mu_c0 and mu_c1
+  # - the parameterization of dividend growth, i.e., mu_d0 and mu_d1.
+  # - the dimension of w, parameter: n_w (used to compute Ew numerically)
+  # In addition, model_solved contains the SDF specification, that includes:
+  # - the vector of prices of risk (alpha)
+  # - the specification of the short-term rate (scalar eta0 and vector eta1).
+  # psi is the log Laplace transform of the state vector w_t; it can itself use
+  #     model as an argument (i.e., model contains the process parameterization).
+  # Ew is the mean of w_t. If NaN, it is computed numerically.
+  # ----------------------------------------------------------------------------
+
+  n_w <- model$n_w
+  Phi <- model$Phi
+
+  mu_d0 <- model$mu_d0
+  mu_d1 <- model$mu_d1
+
+  eta0  <- model$eta0
+  eta1  <- model$eta1
+  alpha <- model$alpha
+
+  # Loop on z_bar: -------------------------------------------------------------
+  z_s_bar <- z_s_bar_ini
+
+  for(iii in 1:nb_loop_z_bar){
+    kappa1s <- c(exp(z_s_bar)/(1 + exp(z_s_bar)))
+    kappa0s <- c(log(1 + exp(z_s_bar)) - kappa1s * z_s_bar)
+
+    # Solving for mu_z1 (fixed-point problem recursively solved) ---------------
+    # Initial value based on VAR representation of w_t process:
+    mu_s1 = solve(diag(n_w) - kappa1s*t(Phi)) %*% (t(Phi) %*% mu_d1 - eta1)
+    for(jjj in 1:nb_loop_mu_z1){
+      u <- matrix(alpha + kappa1s*mu_s1 + mu_d1,ncol=1)
+      psi_w_alpha_plus <- psi(u,model)
+      psi_w_alpha      <- psi(alpha,model)
+      mu_s1 <- psi_w_alpha_plus$a - eta1 - psi_w_alpha$a
+      mu_s0 <- 1/(1-kappa1s)*(psi_w_alpha_plus$b - eta0 - psi_w_alpha$b +
+                                kappa0s + mu_d0)
+    }
+    # update z_bar:
+    z_s_bar <- mu_s0 + t(mu_s1) %*% Ew
+  }
+
+  # Deduce specification of r_{s,t+1}:
+  mu_rs0 <- kappa0s + kappa1s*mu_s0 - mu_s0 + mu_d0
+  mu_rs1 <- - mu_rs1
+  mu_rs2 <- kappa1s * mu_s1 + mu_d1
+
+  model_solved <- model
+
+  model$mu_rs0 <- mu_rs0
+  model$mu_rs1 <- mu_rs1
+  model$mu_rs2 <- mu_rs2
+
+  return(model_solved)
+}
+
 
 psi.BansalShaliastovich <- function (u,model){
   # This function computes the log Laplace transform associated with
   # the model by Bansal and Shaliastovich (2012, RFS)
   n <- dim(model$Pi)[1]
   q <- dim(model$nu)[1]
-  K <- dim(u)[2]
   u.x <- matrix(u[1:n, ], nrow = n)
   u.z <- matrix(u[(n + 1):(n + q), ], nrow = q)
-  u.epsilon <- matrix(u[(n + q + 1):(n + q + 3), ], nrow = 3)
   b <- 0.5 * t(u.x * u.x) %*% matrix(model$chi.0, ncol = 1) +
-    0.5 * t(u.z * u.z) %*% matrix(model$sigma.w^2, ncol = 1) +
-    0.5 * ((t(u.epsilon) %*% model$Omega) * t(u.epsilon)) %*%
-    matrix(1, 3, 1)
+    0.5 * t(u.z * u.z) %*% matrix(model$sigma.w^2, ncol = 1)
   a <- rbind(t(model$Pi) %*% u.x, 0.5 * t(model$nu) %*%
-               model$chi.1 %*% (u.x * u.x) + t(model$nu) %*% u.z, matrix(0,
-                                                                         3, K))
+               model$chi.1 %*% (u.x * u.x) + t(model$nu) %*% u.z)
   return(list(a = a,b = b))
 }
+
+
 
